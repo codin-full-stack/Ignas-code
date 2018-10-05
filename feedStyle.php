fc474f15c8f418df111286a4fa21fd2355ea4cae
Ignitious44 <42798002+Ignitious44@users.noreply.github.com>
2018-10-05 14:50:19 +0300

    penktadienis, frontend..................

--- feedStyle.php
+++ feedStyle.php
@@ -1,18 +1,20 @@
 <?php
-include 'config.php';
+include 'header.php';
 ?>
 
+
+
 <div class="feed">
+
     <div class="page_name">
-        <h1>Straipsnių rodymas</h1>
+    <h1>Straipsnių rodymas</h1>
     </div>
-    <div class="post">
+
+    <article class="post1">
         
         <div class="photo">
             <img src="travel-planning.jpg"/>
-            <div class="photo_tag">
-                <h6>Nuotaukos autorius</h6>
-            </div>
+            <span>Nuotaukos autorius</span>
         </div>
 
         <div class="post_name">
@@ -20,15 +22,38 @@ include 'config.php';
         </div>
 
         <div class="author">
-            <h6>Straipsnio autorius</h6><br>
-            <h6>Data</h6>
+            <span>Straipsnio autorius</span>
+            <span>Data</span>
         </div>
 
         <div class="content">
             <p> Kai esame apriboti atostogų laiko, tada rekomenduojame rinktis ne svajonių šalį, o tą į kurią geras metas keliauti. Rinkdamiesi kryptį pasidomėkite kokios oro sąlygos šalyje, į kurią ruošiatės. Jeigu sunkiai pakeliate karštį ar šaltį, atkreipkite dėmesį ar tokios kraštutinės oro sąlygos nepasitaikys jūsų kelionėje. Tarkim Pietryčių ir Pietų Azijoje vasarą gali būti nepakeliamai karšta, ir beveik kasdien jas plaus tropinės liūtys.</p>
         </div>
 
-    </div>
+    </article>
+
+    <article class="post2">
+        
+        <div class="photo">
+            <img src="travel-planning.jpg"/>
+            <span>Nuotaukos autorius</span>
+        </div>
+
+        <div class="post_name">
+            <h3>Atostogų gidas</h3>
+        </div>
+
+        <div class="author">
+            <span>Straipsnio autorius</span>
+            <span>Data</span>
+        </div>
+
+        <div class="content">
+            <p> Kai esame apriboti atostogų laiko, tada rekomenduojame rinktis ne svajonių šalį, o tą į kurią geras metas keliauti. Rinkdamiesi kryptį pasidomėkite kokios oro sąlygos šalyje, į kurią ruošiatės. Jeigu sunkiai pakeliate karštį ar šaltį, atkreipkite dėmesį ar tokios kraštutinės oro sąlygos nepasitaikys jūsų kelionėje. Tarkim Pietryčių ir Pietų Azijoje vasarą gali būti nepakeliamai karšta, ir beveik kasdien jas plaus tropinės liūtys.</p>
+        </div>
+
+    </article>
+
 </div>
 
 <?php
